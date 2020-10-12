3fc30bf700cdc473ef054627b35d5bcf41f6c5d7
ninjabattler <jaydenrtucker@gmail.com>
2020-10-12 18:14:34 +0000

    Finished the game loop. Users can answer questions, then the next user answers one till one user is out of lives

--- turn.rb
+++ turn.rb
@@ -4,19 +4,44 @@ class Turn
     @p1 = p1
     @p2 = p2
     @current = @p1
-    puts ""
-    puts "#{p1.name} [#{p1.lives}/3] vs #{p2.name} [#{p2.lives}/3]"
-    puts ""
   end
 
   def next_turn
-    puts "-----NEXT TURN-----"
-    q = Question.new(@current)
-    if q.check_answer
+    puts ""
+    puts "#{@p1.name} [#{@p1.lives}/3] vs #{@p2.name} [#{@p2.lives}/3]"
+    question = Question.new(@current)
+    if question.check_answer
       puts "Nice going"
     else 
-      puts "Wrong answer, the answer was #{q.correct}, and now you've lost a life"
-      current.lives -= 1
+      puts "Wrong answer, the answer was #{question.correct}, and now you've lost a life"
+      @current.lives -= 1
+    end
+    puts ""
+    self.change_player
+    self.check_lives
+  end
+
+  def check_lives
+    if @p1.lives == 0
+      puts "-----Game Over-----"
+      puts "#{@p1.name} has lost thy noble life. #{@p2.name} is the victor. The game is over"
+      puts ""
+    elsif @p2.lives == 0
+      puts "-----Game Over-----"
+      puts "#{@p2.name} has lost thy noble life. #{@p1.name} is the victor. The game is over"
+      puts ""
+    else
+      puts "-----NEXT TURN-----"
+      self.next_turn
+    end
+  end
+
+  def change_player
+    case @current
+    when @p1
+      @current = @p2
+    when @p2
+      @current = @p1
     end
   end
 end
